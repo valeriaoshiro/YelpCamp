f4624b83f31234758fe595a983f6c83500a6c594
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-23 16:05:46 -0800

    Seed data, setup comment schema

--- app.js
+++ app.js
@@ -3,23 +3,12 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var seedDB = require('./seeds');
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
-// Campground.create({
-//   name: 'Granite Hill', 
-//   image: 'https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg',
-//   description: 'This is a huge granite hill, no bathroom'
-// }, function(err, campground){
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         console.log("Newly created campground: ");
-//         console.log(campground);
-//     }
-// });
+seedDB();
 
 app.get('/', function(req, res){
     res.render("landing");
@@ -58,7 +47,7 @@ app.post('/campgrounds', function(req, res){
 
 // SHOW 
 app.get('/campgrounds/:id', function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
         if(err) {
             console.log(err);
         } else {
